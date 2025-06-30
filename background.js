6eec17b40d3215c143a95121e33f28f07e3f4a04
Ryan Andersen <ryco117@gmail.com>
2025-06-29 21:50:01 -0700

    Remove homepage annoyances

--- background.js
+++ background.js
@@ -3,37 +3,53 @@ chrome.storage.session.set({ tabStateMap: {} }).catch((error) => {
     console.error(`Failed to initialize yt-rplc state: ${error}`);
 });
 
+const regex = /^https:\/\/www\.youtube\.com(\/watch\?v=(?<video>[a-zA-Z0-9_-]+))?/;
+
 // Listen for tab URL changes.
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete') {
         chrome.storage.session.get('tabStateMap').then((data) => {
             let state = data.tabStateMap[tabId] || {};
 
-            // Exit if the last completed URL on this tab was the current one.
-            if (state.url === tab.url) {
-                return;
-            }
-
             // Update to the current URL.
             state.url = tab.url;
 
             // Update the state for this tab.
             data.tabStateMap[tabId] = state;
-            chrome.storage.session.set(data);
+            chrome.storage.session.set(data).catch((error) => {
+                console.error(`Failed to set yt-rplc state: ${error}`);
+            });
 
-            const regex = /^https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/; // Check if the URL is a YouTube video.
             const match = regex.exec(tab.url);
             if (match) {
-                const capturedGroup = match[1]; // Match the video ID.
-                const newUrl = 'https://www.youtube.com/embed/' + capturedGroup; // Create the new URL.
+                const capturedVideoId = match.groups?.video; // Match the video ID.
+
+                if (!capturedVideoId) {
+                    setTimeout(() => {
+                        chrome.scripting.executeScript({
+                            target: { tabId: tabId },
+                            function: removeHomeAds,
+                        }).catch((error) => {
+                            console.error(`Failed to execute script: ${error}`);
+                        });
+                    }, 1200); // Remove ads after a short delay.
+
+                    return; // If no video ID is captured, exit early.
+                }
+
+                const newUrl = `https://www.youtube.com/embed/${capturedVideoId}`; // Create the new URL.
                 chrome.storage.sync.get('replacePlayer').then((data) => {
                     chrome.scripting.executeScript({
                         target: { tabId: tabId },
                         function: data.replacePlayer ? replacePlayer : redirect,
                         args: [newUrl]
+                    }).catch((error) => {
+                        console.error(`Failed to execute script: ${error}`);
                     });
                 });
             }
+        }).catch((error) => {
+            console.error(`Failed to get yt-rplc state: ${error}`);
         });
     }
 });
@@ -43,7 +59,7 @@ function redirect(newUrl) {
     window.location.replace(newUrl);
 }
 
-// Helper to replace the YouTube player.
+// Helper to replace the player.
 function replacePlayer(newUrl) {
     // Find the relevant elements.
     const div = document.querySelector('#primary-inner');
@@ -58,4 +74,26 @@ function replacePlayer(newUrl) {
 
     // Replace the old player with the embedded one.
     oldPlayer.replaceWith(iframe);
+}
+
+// Helper to remove ads from the home page.
+function removeHomeAds() {
+    if (window.ytRplcAdInterval) {
+        console.log('Ad removal interval already created.');
+        return; // Prevent multiple intervals.
+    }
+
+    window.ytRplcAdInterval = setInterval(() => {
+        console.log('Removing home ads...');
+
+        // Find the relevant elements.
+        document
+            .querySelectorAll('ytd-rich-item-renderer')
+            .forEach(d => {
+                if (d.querySelector('ytd-ad-slot-renderer')) {
+                    console.log('Removing ad');
+                    d.parentNode.removeChild(d); // Remove the ad element.
+                }
+            });
+    }, 3000); // Check every few seconds.
 }
